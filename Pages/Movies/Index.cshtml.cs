9e449dfdc58ab355504bf514ec4547391bcbdf5a
Vladimir <zourawsky@yandex.ru>
2020-08-28 14:53:14 +0300

    Обновление макета (_layout.cshtml)

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using WebApplication_RazorPages_Movie.Data;
 using WebApplication_RazorPages_Movie.Models;
 
 namespace WebApplication_RazorPages_Movie.Pages.Movies
