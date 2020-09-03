cd89b14eff465ded7d0d289fa439b258b14d207f
Vladimir <zourawsky@yandex.ru>
2020-09-03 21:28:04 +0300

    Улучшение вывода на странице Index (использование Аннотаций в классе Модели). Изменение шаблона маршрутизации для Delete Edit Details.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +10,19 @@ namespace WebApplication_RazorPages_Movie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [Display(Name = "Название")]
         public string Title { get; set; }
 
+        [Display(Name = "Дата выпуска")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Жанр")]
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+
+        [Display(Name = "Стоимость")]
         public decimal Price { get; set; }
     }
 }
